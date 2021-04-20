f60db19cdd3991a0f482be9340bcbd4ee9930e31
Pravash2 <pravashbehera2@gmail.com>
2021-04-20 20:32:58 +0530

    commit

--- src/components/Customer_Journey/Schedule/Profile/index.js
+++ src/components/Customer_Journey/Schedule/Profile/index.js
@@ -3,7 +3,10 @@ import Grid from "@material-ui/core/Grid";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
+import Divider from "@material-ui/core/Divider";
+import Menu from "./Menu";
 import Avatar_Pic from "../../../../assets/img/avatar.png";
+import Avatar_Pic2 from "../../../../assets/img/avatar2.png";
 
 const useStyles = makeStyles({
   label: {
@@ -73,6 +76,158 @@ export default function Index() {
           <Button style={{ width: 150, padding: 10 }} variant="outlined">
             Edit
           </Button>
+        </Grid>
+      </Grid>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          marginTop: 50,
+        }}
+      >
+        <div>
+          <p style={{ fontSize: 32, color: "#323232", margin: 0 }}>
+            Participants
+          </p>
+          <p style={{ fontSize: 15, color: "#323232", margin: 0 }}>
+            The following are the list of participants added
+          </p>
+        </div>
+        <div>
+          <Button variant="outlined">Add New</Button>
+        </div>
+      </div>
+      <Divider style={{ margin: "30px 0 0 0" }} />
+      <Grid style={{ margin: 30 }} container spacing={3}>
+        <Grid item lg={4} style={{ display: "flex", alignItems: "center" }}>
+          <Avatar
+            alt="Remy Sharp"
+            src={Avatar_Pic2}
+            style={{
+              width: 100,
+              height: 100,
+              marginRight: 30,
+              marginTop: "-10px",
+            }}
+          />
+          <div>
+            <p
+              style={{
+                fontSize: 15,
+                color: "#323232",
+                margin: 0,
+                fontWeight: 300,
+              }}
+            >
+              Name
+            </p>
+            <p style={{ fontSize: 24, color: "#323232", margin: 0 }}>
+              Gayathri Ajit
+            </p>
+          </div>
+        </Grid>
+        <Grid item lg={3} style={{ display: "flex", alignItems: "center" }}>
+          {" "}
+          <div>
+            <p
+              style={{
+                fontSize: 15,
+                color: "#323232",
+                margin: 0,
+                fontWeight: 300,
+              }}
+            >
+              Gender
+            </p>
+            <p style={{ fontSize: 24, color: "#323232", margin: 0 }}>Female</p>
+          </div>
+        </Grid>
+        <Grid item lg={3} style={{ display: "flex", alignItems: "center" }}>
+          {" "}
+          <div>
+            <p
+              style={{
+                fontSize: 15,
+                color: "#323232",
+                margin: 0,
+                fontWeight: 300,
+              }}
+            >
+              DOB
+            </p>
+            <p style={{ fontSize: 24, color: "#323232", margin: 0 }}>
+              06/12/2005
+            </p>
+          </div>
+        </Grid>
+        <Grid item lg={2} style={{ display: "flex", alignItems: "center" }}>
+          <Menu />
+        </Grid>
+      </Grid>
+      <Grid style={{ margin: 30 }} container spacing={3}>
+        <Grid item lg={4} style={{ display: "flex", alignItems: "center" }}>
+          <Avatar
+            alt="Remy Sharp"
+            src={Avatar_Pic2}
+            style={{
+              width: 100,
+              height: 100,
+              marginRight: 30,
+              marginTop: "0px",
+            }}
+          />
+          <div>
+            <p
+              style={{
+                fontSize: 15,
+                color: "#323232",
+                margin: 0,
+                fontWeight: 300,
+              }}
+            >
+              Name
+            </p>
+            <p style={{ fontSize: 24, color: "#323232", margin: 0 }}>
+              Gayathri Ajit
+            </p>
+          </div>
+        </Grid>
+        <Grid item lg={3} style={{ display: "flex", alignItems: "center" }}>
+          {" "}
+          <div>
+            <p
+              style={{
+                fontSize: 15,
+                color: "#323232",
+                margin: 0,
+                fontWeight: 300,
+              }}
+            >
+              Gender
+            </p>
+            <p style={{ fontSize: 24, color: "#323232", margin: 0 }}>Female</p>
+          </div>
+        </Grid>
+        <Grid item lg={3} style={{ display: "flex", alignItems: "center" }}>
+          {" "}
+          <div>
+            <p
+              style={{
+                fontSize: 15,
+                color: "#323232",
+                margin: 0,
+                fontWeight: 300,
+              }}
+            >
+              DOB
+            </p>
+            <p style={{ fontSize: 24, color: "#323232", margin: 0 }}>
+              06/12/2005
+            </p>
+          </div>
+        </Grid>
+        <Grid item lg={2} style={{ display: "flex", alignItems: "center" }}>
+          <Menu />
         </Grid>
       </Grid>
     </div>
