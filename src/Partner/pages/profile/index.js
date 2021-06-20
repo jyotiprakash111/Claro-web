ee7798ab28385280a1f2da646a16680931a3d636
Pravash2 <pravashbehera2@gmail.com>
2021-06-20 23:55:50 +0530

    commit

--- src/Partner/pages/profile/index.js
+++ src/Partner/pages/profile/index.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
 import SideBar from "../../components/common/SideBar";
 import Header from "../../components/common/HeaderMain";
-import Main from "./main/index";
+import Typography from "@material-ui/core/Typography";
+import Breadcrumbs from "@material-ui/core/Breadcrumbs";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import "./style.css";
 
 export default class index extends Component {
   render() {
@@ -10,7 +15,93 @@ export default class index extends Component {
         <SideBar />
         <div style={{ marginLeft: 300 }}>
           <Header name="User Profile" />
-          <Main />
+          <div id="user_profile">
+            <Breadcrumbs
+              style={{ marginTop: 20 }}
+              separator="›"
+              aria-label="breadcrumb"
+            >
+              <Link color="inherit" href="/">
+                Setup
+              </Link>
+              <Typography color="textPrimary">User profile</Typography>
+            </Breadcrumbs>
+            <p style={{ marginTop: 0 }}>
+              Manage personal details and login credential
+            </p>
+            <Grid container>
+              <Grid lg={7} item>
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  <form style={{ maxWidth: 480, marginTop: 10 }}>
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                      }}
+                    >
+                      <div>
+                        <label for="name">First name </label>
+                        <div style={{ display: "flex" }}>
+                          <input
+                            type="text"
+                            id="name"
+                            name="user_name"
+                            placeholder="E.g. Jane"
+                            style={{ color: "#323232" }}
+                          />
+                        </div>
+                      </div>
+                      <div>
+                        <label for="name">Last name </label>
+                        <div style={{ display: "flex" }}>
+                          <input
+                            type="text"
+                            id="name"
+                            name="user_name"
+                            placeholder="E.g. Doe"
+                            style={{ color: "#323232" }}
+                          />
+                        </div>
+                      </div>
+                    </div>
+
+                    <label for="name">Email address*</label>
+                    <div style={{ display: "flex" }}>
+                      <input
+                        type="text"
+                        id="name"
+                        name="user_name"
+                        placeholder="Email address"
+                        style={{ color: "#323232" }}
+                      />
+                    </div>
+                    <label for="name">Phone number*</label>
+                    <div style={{ display: "flex" }}>
+                      <input
+                        type="text"
+                        id="name"
+                        name="user_name"
+                        placeholder="+91 "
+                        style={{ color: "#323232" }}
+                      />
+                    </div>
+                    <Button
+                      variant="contained"
+                      style={{
+                        color: "#fff",
+                        background: "#FF8021",
+                        width: 200,
+                        borderRadius: 5,
+                        marginTop: 10,
+                      }}
+                    >
+                      Submit
+                    </Button>
+                  </form>
+                </div>
+              </Grid>
+            </Grid>
+          </div>
         </div>
       </div>
     );
--- src/Partner/pages/sales/index.js
+++ src/Partner/pages/sales/index.js
@@ -1,18 +1,129 @@
 import React, { Component } from "react";
 import SideBar from "../../components/common/SideBar";
 import Header from "../../components/common/HeaderMain";
-import Main from "./main/index";
+import TextField from "@material-ui/core/TextField";
+import MenuItem from "@material-ui/core/MenuItem";
+import Button from "@material-ui/core/Button";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import Table from "../../components/Sales/Table";
+import FormDatePicker from "../../components/controls/FormDatePicker";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { FormProvider, useForm } from "react-hook-form";
 
-export default class index extends Component {
-  render() {
-    return (
+const currencies = [
+  {
+    value: "Bhubaneswar",
+    label: "$",
+  },
+  {
+    value: "Cuttack",
+    label: "€",
+  },
+  {
+    value: "Jadev Vihar",
+    label: "฿",
+  },
+  {
+    value: "Forest Park",
+    label: "¥",
+  },
+];
+
+export default function Sales() {
+  const validationSchema = yup.object().shape({});
+  const methods = useForm({
+    resolver: yupResolver(validationSchema),
+  });
+
+  return (
+    <FormProvider {...methods}>
       <div>
         <SideBar />
         <div style={{ marginLeft: 300 }}>
           <Header name="Sales" avatar={true} />
-          <Main />
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              marginRight: 100,
+            }}
+          >
+            <div>
+              <p>Starting From</p>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                <FormDatePicker
+                  name="startTime"
+                  placeholder="DD/MM/YYYY"
+                  required={true}
+                />
+                {/* <div>
+              <DateRangeIcon style={{ color: "#FF8021", fontSize: 30 }} />
+            </div>
+            <p
+              style={{
+                margin: 0,
+                padding: 0,
+                marginTop: -10,
+                fontSize: 18,
+                marginLeft: 10,
+              }}
+            >
+              {" "}
+              7 Feb 2021
+            </p> */}
+              </div>
+            </div>
+            <div>
+              <TextField
+                id="outlined-select-currency"
+                select
+                label="All location"
+                variant="outlined"
+                size="small"
+                style={{ width: 163, height: 44 }}
+              >
+                {currencies.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.value}
+                  </MenuItem>
+                ))}
+              </TextField>
+              <Button
+                variant="outlined"
+                style={{
+                  color: "#FF8021",
+                  borderColor: "#FF8021",
+                  width: 144,
+                  height: 40,
+                  marginLeft: 20,
+                }}
+              >
+                New sales
+              </Button>
+            </div>
+          </div>
+          <div style={{ marginRight: 100, marginTop: 30 }}>
+            <Table />
+            <Button
+              style={{
+                textTransform: "unset",
+                color: "#FF8021",
+                marginTop: 10,
+                marginLeft: -5,
+              }}
+            >
+              Show More <ExpandMoreIcon />
+            </Button>
+          </div>
         </div>
       </div>
-    );
-  }
+    </FormProvider>
+  );
 }
