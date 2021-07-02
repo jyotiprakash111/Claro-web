0bf1d0bce6bae0a8d7e1c3574373a580d6c6377e
pravash <pravashbehera2@gmail.com>
2021-07-02 10:37:53 +0530

    style change of batch Schedule

--- src/Partner/pages/batchSchedule/main/pricing/index.js
+++ src/Partner/pages/batchSchedule/main/pricing/index.js
@@ -1,18 +1,60 @@
 import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
-import { makeStyles } from "@material-ui/core/styles";
-import Table from "./Table";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import useStyles from "./style";
 
-const Customers = () => {
+import IconButton from "@material-ui/core/IconButton";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+
+function createData(name, calories, fat, carbs, protein, amount) {
+  return { name, calories, fat, carbs, protein, amount };
+}
+
+const rows = [
+  createData("xxxx", "dd mmm yyyy", "John Doe", "Paytm", "Paid", "$ xx"),
+  createData("xxxx", "dd mmm yyyy", "John Doe", "Paytm", "Paid", "$ xx"),
+  createData("xxxx", "dd mmm yyyy", "John Doe", "Paytm", "Paid", "$ xx"),
+];
+
+export default function Pricing() {
+  const classes = useStyles();
   return (
-    <div style={{ marginRight: 100 }}>
+    <div className={classes.pricingContainer}>
       <div>
-        <Table />
-        <div style={{ display: "flex", alignItems: "center" }}>
-          <p style={{ color: "#FF8021" }}>Show More</p>
-          <ArrowDropDownIcon style={{ color: "#FF8021", marginTop: 3 }} />
+        <TableContainer component={Paper}>
+          <Table className={classes.table} aria-label="simple table">
+            <TableHead style={{ background: "#F8F8F8" }}>
+              <TableRow>
+                <TableCell>Plan Type</TableCell>
+                <TableCell>Price Name</TableCell>
+                <TableCell>Price</TableCell>
+                <TableCell>Payment Type</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow key={row.name}>
+                  <TableCell>{row.name}</TableCell>
+                  <TableCell>{row.calories}</TableCell>
+                  <TableCell>{row.fat}</TableCell>
+                  <TableCell>{row.carbs}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <div className={classes.pricingContainerDiv}>
+          <p className={classes.pricingContainerDivP}>Show More</p>
+          <ArrowDropDownIcon className={classes.pricingContainerDivIcon} />
         </div>
       </div>
     </div>
   );
-};
-export default Customers;
+}
